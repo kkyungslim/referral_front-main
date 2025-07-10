9d633334255560d9a92f177eb37e04e38e704b99
YunHyunHo <roal3437@naver.com>
2025-07-10 15:32:38 +0900

    facebookpixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from '@/components/Footer';
 import { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/next';
 import Script from 'next/script';
+import FaceBookPixel from '@/components/FaceBookPixel';
 
 const pretendard = localFont({ src: '../assets/PretendardVariable.woff2' });
 
@@ -62,7 +63,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default async function RootLayout({
+export default function RootLayout({
                                            children,
                                          }: Readonly<{
   children: React.ReactNode;
@@ -106,9 +107,31 @@ export default async function RootLayout({
     `,
         }}
       />
+      {/* Meta Pixel Script */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '721640497495619');
+            fbq('track', 'PageView');
+          `}
+      </Script>
     </head>
     <body className={`${pretendard.className} antialiased`}>
+    {/* Meta Pixel noscript fallback */}
+    <noscript>
+      <img height="1" width="1" style={{ display: 'none' }}
+           src="https://www.facebook.com/tr?id=721640497495619&ev=PageView&noscript=1"
+      />
+    </noscript>
     <NextTopLoader color="#ff6900" showSpinner={false} height={2} />
+    <FaceBookPixel/>
     {children}
     <Analytics />
     <Footer />
